35b6a9fd5c114aab016057d945cf553ce758494b
Arman_Dzhambulov <arman1407@ya.ru>
2023-05-22 16:19:17 +0300

    task#4

--- task#4/Program.cs
+++ task#4/Program.cs
@@ -3,34 +3,20 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
-// Console.Clear();
-// Console.Write("Введите число: ");
-// int n = int.Parse(Console.ReadLine()!);
-// Console.Write("Введите число: ");
-// int m = int.Parse(Console.ReadLine()!);
-// Console.Write("Введите число: ");
-// int k = int.Parse(Console.ReadLine()!);
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число: ");
+int k = int.Parse(Console.ReadLine()!);
 
-// int max(int n, int m, int k)
-// {
-//     int result = n;
-//     if(m > result) result = m;
-//     if(k > result) result = k;
-//     return result;
-// }
-//     Console.WriteLine(max);
-
-int Max(int arg1, int arg2, int arg3)
+int Max(int n, int m, int k)
 {
-    int result = arg1;
-    if(arg2 > result) result = arg2;
-    if(arg3 > result) result = arg3;
+    int result = n;
+    if(m > result) result = m;
+    if(k > result) result = k;
     return result;
 }
-int a1 = 15;
-int b1 = 21;
-int c1 = 3;
-
-
-
-
+int max = Max(n, m, k);
+Console.WriteLine(max);
