1383ce29c5e0085dce720d54f7b8d859eed82c7d
Arman_Dzhambulov <arman1407@ya.ru>
2023-05-23 13:34:02 +0300

    task#8

---
+++ task#8/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int count = 0;
+int even = 0;
+int i = (0 < (n + 1));
+{
+    Console.WriteLine();
+
+    even == i % 2;
+    // n % 2 = even;
+    even ++;
+}
+
+    count +=1;
+
+Console.WriteLine(even);
+
+// if (n > m)
+//     Console.WriteLine(n);
+// else if (n < m)
+//     Console.WriteLine(m);
+// else
+//     Console.WriteLine(n);
+
+
+// Console.Clear();
+// int number = new Random().Next(10, 100); // [10; 99]
+// Console.WriteLine(number);
+// int n1 = number / 10;
+// int n2 = number % 10;
+// // 1 == 2, 1 > 2, 1 < 2
+// if (n1 > n2)
+// Console.WriteLine(n1);
+// else
+// Console.WriteLine(n2);
+
+
+
+
+
+
+
