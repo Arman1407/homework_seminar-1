59717e5c0b1edbf67f178c50716e4f8fc01f3519
Arman_Dzhambulov <arman1407@ya.ru>
2023-05-26 11:33:15 +0300

    task#8

--- task#8/Program.cs
+++ task#8/Program.cs
@@ -2,46 +2,21 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
+
+
+// 1 вариант
+
 Console.Clear();
 Console.Write("Введите число: ");
-int n = int.Parse(Console.ReadLine()!);
-int count = 0;
-int even = 0;
-int i = (0 < (n + 1));
+int N = int.Parse(Console.ReadLine()!);
+int n = 2;
+
+if (N > 1)
 {
-    Console.WriteLine();
-
-    even == i % 2;
-    // n % 2 = even;
-    even ++;
+    while (n <= N)
+    {
+        Console.Write(n +  ",");
+        n = n + 2;
+    }
 }
-
-    count +=1;
-
-Console.WriteLine(even);
-
-// if (n > m)
-//     Console.WriteLine(n);
-// else if (n < m)
-//     Console.WriteLine(m);
-// else
-//     Console.WriteLine(n);
-
-
-// Console.Clear();
-// int number = new Random().Next(10, 100); // [10; 99]
-// Console.WriteLine(number);
-// int n1 = number / 10;
-// int n2 = number % 10;
-// // 1 == 2, 1 > 2, 1 < 2
-// if (n1 > n2)
-// Console.WriteLine(n1);
-// else
-// Console.WriteLine(n2);
-
-
-
-
-
-
-
+    
